4bba0640bdc6c9c403643492fd70b9f57e62007d
devshum <vladshumdev@gmail.com>
2023-02-11 21:45:24 +0200

    remove cors

--- src-test/app.js
+++ src-test/app.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
-import cors from 'cors';
 import { taskRouter } from './routers/taskRouter.js';
 
 export const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use('/api/v1/tasks', taskRouter);
 
