91e8c069673b696aa07d670ecfd9cf31cf48e3b6
devshum <vladshumdev@gmail.com>
2023-02-12 15:35:46 +0200

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
 import { taskRouter } from './routers/taskRouter';
+import cors from 'cors';
 
 export const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/v1/tasks', taskRouter);
 
