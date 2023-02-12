2e5ca426e3a4b4d17c1920fae571d46db3b7b81c
devshum <vladshumdev@gmail.com>
2023-02-12 14:48:12 +0200

    remove js module

--- src-test/routers/taskRouter.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { getAllTasks, createTask } from '../controllers/taskController.js';
-
-export const taskRouter = express.Router();
-
-taskRouter.route('/').get(getAllTasks).post(createTask);
